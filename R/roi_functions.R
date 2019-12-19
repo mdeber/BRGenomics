65e398376a6f9de73c99e61a1041914b15099554
Mike DeBerardine <mike.deberardine@gmail.com>
2019-12-19 15:34:32 -0500

    fix errors in genebodies function

--- R/roi_functions.R
+++ R/roi_functions.R
@@ -1,4 +1,19 @@
 
+# for updating documentation:
+#   for when fix_start != fix_end, filters intuitively for minimum window size
+#   if fix_start == fix_end, the size of all the returned ranges will be the same;
+      # for fix_start = "end", must be min_window between annotated
+      #     start and start of interval;
+      # for fix_end = "start", must be min_window between interval end and annotated
+      #     end
+#       but could make it so that...
+#       min_window filters by the length within the annotated region that's
+#       outside the returned range (i.e. for fix_end = "start", the distance
+#       between the interval end to the annotated end; for fix_start = "end",
+#       the distance between the annotated start to the beginning of the interval)
+#
+
+
 #' Extract Genebodies
 #'
 #' Analagous to \code{\link[GenomicRanges:promoters]{GenomicRanges::promoters}},
@@ -62,23 +77,25 @@ genebodies <- function(genelist,
         }
     } else {
         # filter by distance from annotated start to beginning of interval
-        min_width <- min_window + start
+        min_width <- min_window - start
     }
 
     genelist <- subset(genelist, width >= min_width)
 
     # Get genebodies
 
-    starts <- start(resize(genelist, 1, fix = fix_start))
-    ends <- start(resize(genelist, 1, fix = fix_end))
+    # starts are at strand-specific beginnings of the genebodies
+    sense_starts <- start(resize(genelist, 1, fix = fix_start))
+    sense_ends <- start(resize(genelist, 1, fix = fix_end))
 
-    idx_p <- which(as.character(strand(genelist)) == "+")
-    idx_m <- which(as.character(strand(genelist)) == "-")
+    # shift with strand-specificity
+    is_plus <- as.character(strand(genelist)) == "+"
+    sense_starts <- ifelse(is_plus, sense_starts + start, sense_starts - start)
+    sense_ends <- ifelse(is_plus, sense_ends + end, sense_ends - end)
 
-    starts[idx_p] <- starts + start
-    starts[idx_m] <- starts - start
-    ends[idx_p] <- ends + end
-    ends[idx_m] <- ends - end
+    # final starts/ends must be flipped for minus-strand genes
+    starts <- ifelse(is_plus, sense_starts, sense_ends)
+    ends <- ifelse(is_plus, sense_ends, sense_starts)
 
     ranges(genelist) <- IRanges(start = starts, end = ends)
     return(genelist)
