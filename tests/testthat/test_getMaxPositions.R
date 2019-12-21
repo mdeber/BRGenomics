3fbf6d6f045135ecf61a11bbec8f9e5a4330503c
Mike DeBerardine <mike.deberardine@gmail.com>
2019-12-21 13:49:23 -0500

    add tests for getMaxPositions

---
+++ tests/testthat/test_getMaxPositions.R
@@ -0,0 +1,66 @@
+context("Finding max-signal positions within regions of interest")
+library(BRGenomics)
+
+data(PROseq)
+data("txs_dm6_chr4")
+
+test_that("Empty input regions returns empty GRanges", {
+    out <- getMaxPositionsBySignal(regions.gr = txs_dm6_chr4[0],
+                                   dataset.gr = PROseq)
+
+    expect_is(out, "GRanges")
+    expect_equal(length(out), 0)
+    expect_equivalent(names(mcols(out)), names(mcols(txs_dm6_chr4)))
+})
+
+test_that("keep.score with empty input regions is properly formatted", {
+    # should add metadata for MaxSiteScore
+    out <- getMaxPositionsBySignal(regions.gr = txs_dm6_chr4[0],
+                                   dataset.gr = PROseq,
+                                   keep.score = TRUE)
+    expect_equal(ncol(mcols(out)), ncol(mcols(txs_dm6_chr4)) + 1)
+})
+
+test_that("No signal overlapping regions returns formatted, empty GRanges", {
+    # 3 regions without overlapping signal
+    out <- getMaxPositionsBySignal(regions.gr = txs_dm6_chr4[c(24, 60, 62)],
+                                   dataset.gr = PROseq)
+    expect_is(out, "GRanges")
+    expect_equal(length(out), 0)
+    expect_equivalent(names(mcols(out)), names(mcols(txs_dm6_chr4)))
+})
+
+test_regions <- unique(txs_dm6_chr4)[1:10]
+test_regions_promoter <- promoters(test_regions, 0, 300)
+
+test_that("Max sites found for regions with signal", {
+    out <- getMaxPositionsBySignal(regions.gr = test_regions_promoter,
+                                   dataset.gr = PROseq)
+    expect_equal(length(out), length(subsetByOverlaps(test_regions_promoter,
+                                                      PROseq)))
+    expect_equivalent(start(out)[1:4], c(42799, 44801, 69402, 69402))
+    expect_true(all(width(out) == 1))
+})
+
+test_that("Correct signal metadata added for regions with signal", {
+    out <- getMaxPositionsBySignal(regions.gr = test_regions_promoter,
+                                   dataset.gr = PROseq,
+                                   keep.score = TRUE)
+    # two repeated tests
+    expect_equivalent(start(out)[1:4], c(42799, 44801, 69402, 69402))
+    expect_equal(ncol(mcols(out)), ncol(mcols(txs_dm6_chr4)) + 1)
+
+    score_col <- which(!names(mcols(out)) %in% names(mcols(txs_dm6_chr4)))
+    expect_equivalent(mcols(out)[1:4, score_col], c(3, 1, 122, 122))
+})
+
+test_that("Max sites found with multi-width input", {
+    out <- getMaxPositionsBySignal(regions.gr = test_regions,
+                                   dataset.gr = PROseq)
+    expect_is(out, "GRanges")
+    expect_equal(length(out), length(test_regions)) # all have signal
+    expect_equivalent(start(out)[1:4], c(1296, 42799, 45916, 60310))
+})
+
+
+
