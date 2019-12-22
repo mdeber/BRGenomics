33bee9b09e7745cd807811b6cd503d6fa5be0a9e
Mike DeBerardine <mike.deberardine@gmail.com>
2019-12-21 19:31:18 -0500

    modified signal counting functions to support varied-width ROIs, simultaneous aggregation of multiple fields, and arbitrary bin aggregation functions

--- R/signal_counting.R
+++ R/signal_counting.R
@@ -10,12 +10,16 @@
 #' @param field The metadata field of \code{dataset.gr} to be counted. If
 #'   \code{length(field) > 1}, a dataframe is returned containing the counts for
 #'   each region in each field.
+#' @param ncores Multiple cores can only be used if \code{length(field) > 1}.
 #'
 #' @return Returns a vector the same length as \code{regions.gr} containing
 #'   signal found in each range.
 #' @author Mike DeBerardine
 #' @export
-getCountsByRegions <- function(dataset.gr, regions.gr, field = "score") {
+getCountsByRegions <- function(dataset.gr,
+                               regions.gr,
+                               field = "score",
+                               ncores = detectCores()) {
     if (length(field) == 1) {
         hits <- findOverlaps(regions.gr, dataset.gr)
         counts <- aggregate(mcols(dataset.gr)[[field]][hits@to],
@@ -26,9 +30,12 @@ getCountsByRegions <- function(dataset.gr, regions.gr, field = "score") {
         counts.all[counts$gene.idx] <- counts$signal
         return(counts.all)
     } else {
-        counts <- lapply(field, function(i) getCountsByRegions(dataset.gr,
-                                                               regions.gr,
-                                                               field = i))
+        # recursive call
+        counts <- mclapply(field,
+                           function(i) getCountsByRegions(dataset.gr,
+                                                          regions.gr,
+                                                          field = i),
+                           mc.cores = ncores)
         names(counts) <- field
         return(as.data.frame(counts))
     }
@@ -44,29 +51,78 @@ getCountsByRegions <- function(dataset.gr, regions.gr, field = "score") {
 #'   All ranges must have the same width!
 #' @param binsize Size of bins (in bp) to use for counting within each range of
 #'   \code{regions.gr}. Note that counts will \emph{not} be length-normalized.
-#' @param field The metadata field of \code{dataset.gr} to be counted.
-#' @param remove_empty Logical indicating whether regions without signal should
-#'   be removed.
+#' @param bin_FUN If \code{binsize > 1}, the function used to aggregate the
+#'   signal at each base within each bin. The default is to sum the signal in
+#'   each bin, but any function operating on a numeric vector can be used.
+#' @param simplify_multi_widths A string indicating the output format if the
+#'   ranges in \code{regions.gr} have variable widths. Default = \code{"list"}.
+#'   See details below.
+#' @param field The metadata field of \code{dataset.gr} to be counted. If
+#'   \code{length(field) > 1}, the output is a list whose elements contain the
+#'   output for generated each field.
+#' @param ncores Multiple cores can only be used if \code{length(field) > 1}.
 #'
-#' @return A matrix containing a row for each range in regions.gr, and a column
-#'   for each bin.
+#' @details If the widths of all ranges in \code{regions.gr} are equal, a matrix
+#'   is returned containing a row for each range in \code{regions.gr}, and a
+#'   column for each bin. For input \code{regions.gr} with varying widths,
+#'   setting \code{simplify_multi_widths = "list"} will output a list of
+#'   variable-length vectors, with each vector corresponding to an input region.
+#'   If \code{simplify_multi_widths = "pad zero"} or \code{simplify_multi_widths
+#'   = "pad NA"}, the output is a matrix containing a row for each range in
+#'   \code{regions.gr}, and a column for each position in each range. The number
+#'   of columns is determined by the largest range in \code{regions.gr}, and
+#'   columns corresponding to positions outside of each range are either set to
+#'   \code{0} or \code{NA}, depending on the argument.
+#'
 #' @author Mike DeBerardine
 #' @export
 getCountsByPositions <- function(dataset.gr,
                                  regions.gr,
                                  binsize = 1,
+                                 bin_FUN = sum,
+                                 simplify_multi_widths = c("list",
+                                                           "pad zero",
+                                                           "pad NA"),
                                  field = "score",
-                                 remove_empty = FALSE) {
-    if (length(unique(width(regions.gr))) > 1) {
-        stop(message = "Not all ranges in regions.gr are the same width")
-        return(geterrmessage())
+                                 ncores = detectCores()) {
+
+    # function makes practical use of single-width dataset.gr, but the output
+    # is always valid regardless of the input data type
+    if (any(width(dataset.gr) != 1)) dataset.gr <- makeGRangesBPres(dataset.gr)
+
+    multi_width <- length(unique(width(regions.gr))) > 1 # (logical)
+
+    if (length(field) > 1) {
+        # recursive call
+        reslist <- mclapply(field, function(i) {
+            getCountsByPositions(dataset.gr = dataset.gr,
+                                 regions.gr = regions.gr,
+                                 binsize = binsize,
+                                 bin_FUN = bin_FUN,
+                                 simplify_multi_widths = simplify_multi_widths,
+                                 field = i)
+        }, mc.cores = ncores)
+        names(reslist) <- field
+        return(reslist)
     }
 
-    # function requires widths of ranges in dataset.gr are also equal to 1
-    if (length(unique(width(dataset.gr))) > 1)
-        dataset.gr <- makeGRangesBPres(dataset.gr)
+    if (multi_width) {
 
-    if (remove_empty)  regions.gr <- subsetByOverlaps(regions.gr, dataset.gr)
+        # check 'simplify_multi_widths' argument
+        if (missing(simplify_multi_widths))  simplify_multi_widths <- "list"
+        if (!simplify_multi_widths %in% c("list", "pad zero", "pad NA")) {
+            stop(message = .nicemsg("regions.gr has multiple widths, but an
+                                    invalid argument for simplify_multi_widths
+                                    was given. See documentation"))
+            return(geterrmessage())
+        }
+
+        # expand all regions to be the same width
+        widths <- width(regions.gr) # save widths
+        suppressWarnings(
+            regions.gr <- resize(regions.gr, max(widths))
+        )
+    }
 
     # initialize signal matrix of dim = (region, position within region)
     mat <- matrix(0, length(regions.gr), unique(width(regions.gr)))
@@ -83,8 +139,32 @@ getCountsByPositions <- function(dataset.gr,
     mat[cbind(x, y)] <- z
 
     if (binsize > 1) {
-        mat <- apply(mat, 1, .binVector, binsize = binsize)
+        mat <- apply(mat, 1, function(x) .binVector(x, binsize = binsize,
+                                                    FUN = bin_FUN))
         mat <- t(mat) # apply will cbind rather than rbind
+    }
+
+    if (multi_width) {
+        nbins_i <- floor(widths / binsize) # number of bins within each region
+
+        if (simplify_multi_widths == "list") {
+
+            return(mapply(function(i, nbin) mat[i, 1:nbin],
+                          1:nrow(mat), nbins_i))
+
+        } else {
+            arridx_pad <- vapply(nbins_i,
+                                 function(nbin) seq_len(ncol(mat)) > nbin,
+                                 logical(ncol(mat)))
+            arridx_pad <- t(arridx_pad) # sapply/vapply cbinds the rows
+            arridx_pad <- which(arridx_pad, arr.ind = TRUE)
+
+            if (simplify_multi_widths == "pad zero") {
+                mat[arridx_pad] <- 0
+            } else {
+                mat[arridx_pad] <- NA
+            }
+        }
     }
 
     return(mat)
@@ -139,17 +219,17 @@ getPausingIndices <- function(dataset.gr,
                                     regions.gr = genebodies.gr,
                                     field = field)
 
+    if (length_normalize) {
+        counts_pr <- counts_pr / width(promoters.gr)
+        counts_gb <- counts_gb / width(genebodies.gr)
+    }
+
     if (remove_empty) {
         idx_pr <- which(counts_pr != 0)
         idx_gb <- which(counts_gb != 0)
         idx <- intersect(idx_pr, idx_gb)
         counts_pr <- counts_pr[idx]
         counts_gb <- counts_gb[idx]
-    }
-
-    if (length_normalize) {
-        counts_pr <- counts_pr / width(promoters.gr)
-        counts_gb <- counts_gb / width(genebodies.gr)
     }
 
     return(counts_pr / counts_gb)
--- tests/testthat/test_getCounts_getPausingIndices.R
+++ tests/testthat/test_getCounts_getPausingIndices.R
@@ -22,8 +22,9 @@ test_that("signal counts works with alternative metadata fields", {
                                                       field = "signal"))
 })
 
+ps_rename$posnum <- seq_along(ps_rename)
+
 test_that("signal counts work over multiple metadata fields", {
-    ps_rename$posnum <- seq_along(ps_rename)
     counts_multiple <- getCountsByRegions(ps_rename, txs_dm6_chr4,
                                           field = c("signal", "posnum"))
     expect_is(counts_multiple, "data.frame")
@@ -35,10 +36,92 @@ test_that("signal counts work over multiple metadata fields", {
 # getCountsByPositions ----------------------------------------------------
 context("Counting signal at positions within regions")
 
+txs_pr <- promoters(txs_dm6_chr4, 0, 100)
+countsmat <- getCountsByPositions(PROseq, txs_pr)
 
+test_that("simple counts matrix correctly calculated", {
+    expect_is(countsmat, "matrix")
+    expect_equivalent(dim(countsmat), c(length(txs_dm6_chr4), 100))
+    expect_equivalent(rowSums(countsmat), getCountsByRegions(PROseq, txs_pr))
+})
+
+test_that("simple counts matrix works over multiple metadata fields", {
+    fieldcounts <- getCountsByPositions(ps_rename, txs_pr,
+                                       field = c("signal", "posnum"))
+    expect_is(fieldcounts, "list")
+    expect_equivalent(names(fieldcounts), c("signal", "posnum"))
+    expect_is(fieldcounts[[1]], "matrix")
+    expect_is(fieldcounts[[2]], "matrix")
+    expect_equivalent(fieldcounts[[1]], countsmat)
+})
+
+
+countslist <- getCountsByPositions(PROseq, txs_dm6_chr4)
+
+test_that("can get list from multi-width counts matrix", {
+    expect_is(countslist, "list")
+    expect_equal(length(countslist), nrow(countsmat))
+    expect_equivalent(width(txs_dm6_chr4), lengths(countslist))
+
+    idx <- which(width(txs_dm6_chr4) >= 100)
+    expect_equivalent(countsmat[idx, ],
+                      t(sapply(countslist[idx], "[", 1:100)))
+})
+
+test_that("can get 0-padded counts matrix for multi-width regions", {
+    padmat <- getCountsByPositions(PROseq, txs_dm6_chr4,
+                                   simplify_multi_widths = "pad zero")
+    expect_is(padmat, "matrix")
+    expect_equivalent(dim(padmat),
+                      c(length(txs_dm6_chr4), max(width(txs_dm6_chr4))))
+    expect_equivalent(sapply(countslist, sum), rowSums(padmat))
+})
+
+test_that("can get NA-padded counts matrix for multi-width regions", {
+    padmat <- getCountsByPositions(PROseq, txs_dm6_chr4,
+                                   simplify_multi_widths = "pad NA")
+    expect_is(padmat, "matrix")
+    expect_equal(sum(is.na(rowSums(padmat))), length(txs_dm6_chr4) - 1)
+})
+
+
+test_that("can perform arbitrary binning operations on count matrix", {
+    binsums <- getCountsByPositions(PROseq, txs_pr, binsize = 10,
+                                    bin_FUN = sum)
+    expect_is(binsums, "matrix")
+    expect_equivalent(rowSums(binsums), rowSums(countsmat))
+    expect_equivalent(dim(binsums), c(length(txs_dm6_chr4), 10))
+
+    binmeans <- getCountsByPositions(PROseq, txs_pr, binsize = 10,
+                                     bin_FUN = mean)
+    expect_is(binmeans, "matrix")
+    expect_equivalent(which(binsums == 0), which(binmeans == 0))
+    expect_true(sum(binsums) != sum(binmeans))
+
+    binmedians <- getCountsByPositions(PROseq, txs_pr, binsize = 10,
+                                       bin_FUN = function(x) quantile(x, 0.5))
+    expect_is(binmedians, "matrix")
+    expect_equivalent(dim(binmedians), dim(binsums))
+    expect_false(all(rowSums(binmedians) == 0))
+    expect_false(all((binmedians == 0) == (binmeans == 0)))
+})
 
 
 # Calculating pause indices -----------------------------------------------
 context("Calculating pausing indices")
+
+txs_gb <- flank(txs_pr, 500, start = FALSE)
+
+test_that("can calculate pausing indices", {
+    pidx <- getPausingIndices(PROseq, txs_pr, txs_gb)
+
+    expect_is(pidx, "numeric")
+    expect_equivalent(round(pidx[1:4]), c(0, 2, 3, 0))
+})
+
+test_that("error if regions.pr not matching regions.gb", {
+    expect_error(getPausingIndices(PROseq, txs_pr, txs_gb[1:10]))
+})
+
 
 
