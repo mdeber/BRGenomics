4d9d13c877b4b1fdd8ddff2d5e801496df749ea0
Mike DeBerardine <mike.deberardine@gmail.com>
2019-12-26 15:26:53 -0500

    test stranded coverage without weighting

--- tests/testthat/test_getStrandedCoverage_makeGRangesBPres.R
+++ tests/testthat/test_getStrandedCoverage_makeGRangesBPres.R
@@ -1,12 +1,12 @@
-context("Functions on dataset GRanges")
+# Testing getStrandedCoverage ---------------------------------------------
+
+context("getting stranded coverage")
 library(BRGenomics)
 
 data("PROseq_paired")
 paired_3p_ends <- resize(PROseq_paired, 1, fix = "end")
 paired_3p_cov <- getStrandedCoverage(paired_3p_ends)
 
-
-# Testing getStrandedCoverage ---------------------------------------------
 
 test_that("Stranded coverage makes stranded GRanges", {
     expect_is(paired_3p_cov, "GRanges")
@@ -37,9 +37,31 @@ test_that("Stranded coverage correctly calculated", {
     expect_equal(sum_3pcov_by_width, 73011)
 })
 
+test_that("stranded coverage correct without any weighting", {
+    reads_gr <- rep(paired_3p_ends, paired_3p_ends$score)
+    mcols(reads_gr) <- NULL
+    reads_cov <- getStrandedCoverage(reads_gr, field = NULL)
+
+    expect_equivalent(reads_cov, paired_3p_cov)
+    expect_error(getStrandedCoverage(reads_gr))
+})
+
+test_that("Empty input returns GRanges", {
+    null_cov <- getStrandedCoverage(paired_3p_ends[0])
+    expect_is(null_cov, "GRanges")
+    expect_equal(length(null_cov), 0)
+    expect_equivalent(seqinfo(paired_3p_ends), seqinfo(null_cov))
+    expect_equivalent(mcols(paired_3p_ends[0]), mcols(null_cov))
+})
+
+test_that("error on invalid field", {
+    expect_error(getStrandedCoverage(paired_3p_ends, "wrongname"))
+})
 
 
 # Testing makeGRangesBPres ------------------------------------------------
+
+context("making GRanges BP-resolution")
 
 test_that("disJoint input for makeGRangesBPres makes error", {
     expect_error(makeGRangesBPres(PROseq_paired))
