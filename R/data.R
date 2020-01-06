cf04031b406005b4d798475a9e75eaab8788dc0d
Mike DeBerardine <mike.deberardine@gmail.com>
2020-01-06 15:13:51 -0500

    combined PRO-seq data documentation

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' PRO-seq data from Drosophila S2 cells
 #'
-#' PRO-seq data of Drosophila S2 cells, chromosome 4.
+#' PRO-seq data from chromosome 4 of Drosophila S2 cells.
 #'
-#' Hojoong Kwak, Nicholas J. Fuda, Leighton J. Core, John T. Lis (2013).
-#' Precise Maps of RNA Polymerase Reveal How Promoters Direct Initiation and
-#' Pausing. Science, 339(6122), 950–953.
-#' \url{https://doi.org/10.1126/science.1229386}
+#' @references Hojoong Kwak, Nicholas J. Fuda, Leighton J. Core, John T. Lis
+#'   (2013). Precise Maps of RNA Polymerase Reveal How Promoters Direct
+#'   Initiation and Pausing. Science, 339(6122), 950–953.
+#'   \url{https://doi.org/10.1126/science.1229386}
 #'
 #' @format A disjoint GRanges object with 47533 ranges with 1 metadata column:
 #' \describe{
@@ -13,28 +13,16 @@
 #'   ...
 #' }
 #' @source GEO Accession GSM1032758, run SRR611828.
-#' @usage data(PROseq)
 #' @importFrom utils data
+#'
+#' @name PROseq-data
+
+#' @rdname PROseq-data
+#' @usage data(PROseq)
 "PROseq"
 
-
-#' Paired PRO-seq data from Drosophila S2 cells
-#'
-#' PRO-seq data of Drosophila S2 cells, chromosome 4. Entire mapped reads kept.
-#'
-#' Hojoong Kwak, Nicholas J. Fuda, Leighton J. Core, John T. Lis (2013).
-#' Precise Maps of RNA Polymerase Reveal How Promoters Direct Initiation and
-#' Pausing. Science, 339(6122), 950–953.
-#' \url{https://doi.org/10.1126/science.1229386}
-#'
-#' @format A GRanges object with 52464 ranges with 1 metadata column:
-#' \describe{
-#'   \item{score}{number of reads sharing the same mapped 5' and 3' ends}
-#'   ...
-#' }
-#' @source GEO Accession GSM1032758, run SRR611828.
+#' @rdname PROseq-data
 #' @usage data(PROseq_paired)
-#' @importFrom utils data
 "PROseq_paired"
 
 
