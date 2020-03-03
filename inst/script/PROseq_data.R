e331601cc469e81c50c88405dfcde6e92819c0d9
Mike DeBerardine <mike.deberardine@gmail.com>
2020-03-02 20:25:22 -0500

    changed all included data: made bam file <200 reads, and made bigWig and bedGraphs match; added xz compression to included RData files; removed run-on base from all included data (except the bam file)

--- inst/script/PROseq_data.R
+++ inst/script/PROseq_data.R
@@ -8,68 +8,30 @@
 #'
 #' Base-pair resolution PRO-seq was originally sequenced on a GAIIx by the
 #' authors. Unprocessed reads were downloaded from GEO, adapters were trimmed
-#' using Cutadapt, and trimmed reads were aligned using bowtie2 in
-#' high-sensitivity end-to-end alignment mode.
+#' using fastx clipper, and trimmed reads were aligned using bowtie2 in
+#' end-to-end alignment mode, with options set to "sensitive".
 #'
-#' To generate bigWig and bedGraph files, Bam files were processed in R using
-#' Rsamtools and GenomeAlignments, and reads were processed using GenomicRanges.
-#' For all processing, reads were first reverse complemented by inverting the
-#' strand of the GRanges object.
+#' After sorting, the bam file was modified to contain only the first 100 plus
+#' strand reads (biological plus strand, i.e. the reverse of the bam flag, as
+#' PRO-seq is sequenced as the reverse complement), and the last 100 minus
+#' strand reads.
 #'
-#' bedGraph files with paired 5'- and 3'-end information were created in which
-#' each range represents a unique pairing of 5' and 3' ends in the sequencing,
-#' and the score metadata column represents the number of times that pairing was
-#' observed. bigWig files containing only coverage of the 3' ends were created
-#' by truncating reads to their 3' ends, and IRanges was used to calculate
-#' coverage. For both the paired bedGraph files and 3' end bigWig files,
+#' To generate bigWig and bedGraph files, the bam file was processed in R using
+#' BRGenomics (Rsamtools and GenomeAlignments). bedGraph files with paired 5'-
+#' and 3'-end information were created in which each range represents a unique
+#' pairing of 5' and 3' ends in the sequencing, and the score metadata column
+#' represents the number of times that pairing was observed. bigWig files
+#' containing only coverage of the 3' ends were created by truncating reads to
+#' their 3' ends. For both the paired bedGraph files and 3' end bigWig files,
 #' rtracklayer was used to export only the reads mapping to chromosome 4.
 
-# infile <- BamFile(paste0(opt$input_dir, bam_file))
-# param <- ScanBamParam(mapqFilter = opt$quality)
-# alignment = readGAlignments(infile,
-#                             use.names = F,
-#                             param = param)
-# reads <- GRanges(alignment)
+# using BRGenomics import_bam and rtracklayer export functions:
+# library(BRGenomics)
+# reads <- import_bam(path_to_bam_file, mapq = 20,
+#                     revcomp = TRUE, shift = c(0, -1))
+# export.bedGraph(subset(reads, strand == "+"), "PROseq_dm6_chr4_plus.bedGraph")
+# export.bedGraph(subset(reads, strand == "-"), "PROseq_dm6_chr4_minus.bedGraph")
 #
-# # take chromosome 4 only
-# reads <- subset(reads, seqnames == "chr4")
-#
-# # PRO-seq reads must be reverse complemented
-# strand(reads) = ifelse(strand(reads) == '+', '-', '+')
-#
-# # Get 3' ends for PRO-seq
-# reads.3p = GenomicRanges::resize(reads, width = 1, fix = "end")
-#
-# # Collapse identical molecules (for paired and 3'-ends-only)
-# # score for number of identical bits of information
-# collapse_reads <- function(reads) {
-#     reads <- sort(reads)
-#     reads.unique <- unique(reads)
-#     score(reads.unique) <- countOverlaps(reads.unique, reads, type = "equal")
-#     return(reads.unique)
-# }
-# reads <- collapse_reads(reads)
-# reads.3p <- collapse_reads(reads.3p)
-#
-# # separate by strand
-# reads.p <- reads[which(strand(reads)=="+")]
-# reads.m <- reads[which(strand(reads)=="-")]
-#
-# reads.3p.p <- reads[which(strand(reads.3p)=="+")]
-# reads.3p.m <- reads[which(strand(reads.3p)=="-")]
-#
-# # make scores for minus strand reads negative
-# score(reads.m) <- -(score(reads.m))
-# score(reads.3p.m) <- -(score(reads.3p.m))
-#
-# rtracklayer::export.bedGraph(reads.p, "PROseq_dm6_chr4_plus.bedGraph")
-# rtracklayer::export.bedGraph(reads.m, "PROseq_dm6_chr4_plus.bedGraph")
-#
-# rtracklayer::export.bw(reads.3p.p, "PROseq_dm6_chr4_plus.bw")
-# rtracklayer::export.bw(reads.3p.m, "PROseq_dm6_chr4_plus.bw")
-
-
-
-
-
-
+# ps <- import_bam_PROseq(path_to_bam_file, mapq = 20)
+# export.bedGraph(subset(ps, strand == "+"), "PROseq_dm6_chr4_plus.bw")
+# export.bedGraph(subset(ps, strand == "-"), "PROseq_dm6_chr4_minus.bw")
